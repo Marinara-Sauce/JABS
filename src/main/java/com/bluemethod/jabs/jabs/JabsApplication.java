81c5440a863e9f1a83c949bd6b06e10e7438a600
Dan Bliss <danbliss14027@gmail.com>
2022-05-10 13:46:26 -0400

    Removed an unused and broken class, going to start using branches now

--- src/main/java/com/bluemethod/jabs/jabs/JabsApplication.java
+++ src/main/java/com/bluemethod/jabs/jabs/JabsApplication.java
@@ -1,7 +1,5 @@
 package com.bluemethod.jabs.jabs;
 
-import com.bluemethod.jabs.jabs.utils.HTTPRequests;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JabsApplication {
 
 	public static void main(String[] args) {
-		HTTPRequests.getSteamProfile("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", "22240C044D4271252789763016CE9F45", "76561198169279086");
+		//HTTPRequests.getSteamProfile("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", "22240C044D4271252789763016CE9F45", "76561198169279086");
 		SpringApplication.run(JabsApplication.class, args);
 	}
 
