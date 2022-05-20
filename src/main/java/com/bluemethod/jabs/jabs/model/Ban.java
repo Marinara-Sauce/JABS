6a7749641987f62f203d9b87bedbb9c9179c6641
Dan Bliss <danbliss14027@gmail.com>
2022-05-20 13:24:08 -0400

    Began working on bans

---
+++ src/main/java/com/bluemethod/jabs/jabs/model/Ban.java
@@ -0,0 +1,17 @@
+package com.bluemethod.jabs.jabs.model;
+
+import java.util.Date;
+
+/**
+ * This class handles bans, and provides all the information needed for one
+ */
+public class Ban 
+{
+    //Is the ban a perma ban
+    private boolean permaBan;
+
+    //The date the ban expires on
+    private Date liftedDate;
+
+    
+}
--- src/main/java/com/bluemethod/jabs/jabs/model/User.java
+++ src/main/java/com/bluemethod/jabs/jabs/model/User.java
@@ -3,7 +3,6 @@ package com.bluemethod.jabs.jabs.model;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
